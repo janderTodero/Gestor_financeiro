b65f526ae756fcda34071d1b58d27a7412f8e295
jandertodero <jandertodero@gmail.com>
2025-04-14 12:30:53 -0300

    Backup antes de adicionar nova migration

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,4 +6,5 @@ pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "chart.js", to: "https://ga.jspm.io/npm:chart.js@4.4.0/dist/chart.js"
-
+pin "chartkick", to: "chartkick.js"
+pin "Chart.bundle", to: "Chart.bundle.js"

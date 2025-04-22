189208174afa4eddf91ccfbbd1649eb54fb78350
jandertodero <jandertodero@gmail.com>
2025-04-22 12:09:27 -0300

    Adding RSpec for tests

---
+++ spec/models/transaction_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe Transaction, type: :model do
+  context 'Validações' do 
+    it 'é válido com atributos válidos' do
+      user = User.create!(email: 'teste@email.com', password: '123456')
+      transaction = Transaction.new(
+        title: 'Salário',
+        amount: 5000.0,
+        description: 'Salário de agosto',
+        transaction_type: 'entrada',
+        category: 'salario',
+        user: user
+      )
+
+      expect(transaction).to be_valid
+    end
+  end
+end
